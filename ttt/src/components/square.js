f6d8a1eb2787c3903170f4d0d1b933f4c66c153b
Brad Smith <brad@smithwebtek.com>
2019-08-13 20:59:39 -0400

    Update Keiko Corp and TTT

---
+++ ttt/src/components/square.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <biutton className="square">
+        {/* TODO */}
+      </biutton>
+    )
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div >
+    )
+  }
+}
+
+export default { Board, Square }
